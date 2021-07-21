850b2c5c3027b375536ead9cffa9988caa7e0e10
sumedha <sumedhaskumaron14@gmail.com>
2021-07-22 00:06:10 +1000

    Implemented file handling

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class Controller {
+import Model.Payment;
+import Model.Reversal;
+import Model.Transaction;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Controller {
+    ArrayList<Transaction> transactions;
+
+    public Controller() {
+        this.transactions = new ArrayList<>();
+    }
+
+    public void processCSVData(String filePath, DateTimeFormatter format)throws Exception {
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        //Open the file at specified location in read mode
+        Scanner file;
+        try {
+            file = new Scanner(new File(filePath));
+
+            file.useDelimiter("\n");
+
+            //Process each record in csv file, create an object of desired type and add it to transactions array
+            while (file.hasNext()){
+                String line = file.next();
+                String[] words = line.split(",");
+
+                String transactionID = words[0].trim();
+                String fromAccountID = words[1].trim();
+                String toAccoundID = words[2].trim();
+                String dateTimeString = words[3].trim();
+                LocalDateTime createdAt = LocalDateTime.parse(dateTimeString.trim(), format);
+                double amount = Double.parseDouble(words[4].trim());
+
+                if (words[5].trim().compareTo("PAYMENT")==0){
+                    Payment p = new Payment(transactionID, fromAccountID, toAccoundID, createdAt, amount);
+                    transactions.add(p);
+                }
+
+                else {
+                    String reverseTransactionID = words[6].trim();
+                    Reversal r = new Reversal(transactionID, fromAccountID, toAccoundID, createdAt, amount, reverseTransactionID);
+                    transactions.add(r);
+
+                    for (int i = 0; i < transactions.size(); i++) {
+                        Transaction t = transactions.get(i);
+                        if (t.getTransactionID().compareTo(reverseTransactionID)==0 && (t instanceof Payment)){
+                            Payment p = (Payment) t;
+                            p.setHasReversal(true);
+                        }
+                    }
+                }
+
+            }
+
+            //end of processing transactions and loading them into program memory
+            this.transactions = transactions;
+
+            System.out.println("Size" + this.transactions.size());
+        } catch (Exception e) {
+            System.out.println("File not found");
+            throw new Exception("Exception occured");
+        }
+    }
+
 }
